5c643b0372e389d18d2aa531766a2e5fe11e2ac3
Jeff Mesnil <jmesnil@redhat.com>
2024-07-29 17:55:31 +0200

    Update exporter main code
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- insights-runtime-exporter/README.md
+++ insights-runtime-exporter/README.md
@@ -1,10 +1,10 @@
 # Insights Runtime Exporter
 
-The exporter is a HTTP server that acts as the facade for the insights-runtime-extractor.
+The `exporter` is a HTTP server that acts as the façade for the insights-runtime-extractor.
 
-Upon request of a `GET /gather-runtime-info`, it opens a TCP connection to the `extractor` (running on `localhost:3000`)
+Upon request of a `GET /gather-runtime-info`, it opens a TCP connection to the `extractor` (running on `127.0.0.1:3000`)
 The `extractor` replies with a directory path that contains the raw runtime information.
-The `exported` reads files in that directory and generates a JSON payload that is sent back with the HTTP response
+The `exporter` reads files in that directory and generates a JSON payload that is sent back with the HTTP response.
 It then deletes the directory that it read from.
 
 # Build
@@ -20,7 +20,7 @@ Run `make test` to run the tests
 ```mermaid
 sequenceDiagram
   participant http-client
-  box insights-runtime-extractor [pod]
+  box insights-runtime-extractor
   participant exporter
   participant V as volume
   participant extractor
@@ -30,11 +30,15 @@ Note over exporter: HTTP server listening on :8000
 Note over extractor: TCP server bound to 127.0.0.1:3000
 http-client->>exporter: GET /gather-runtime-info
 activate exporter
-exporter->>extractor: TCP trigger an extraction
+exporter->>extractor: TCP connection to trigger the extractor
+activate extractor
+extractor->>extractor: extract runtime info
+extractor->>V: store the info in the timestamped dir
 extractor->>exporter: path of the timestamped dir
+deactivate extractor
 exporter->>V: read content of the dir
 exporter->>exporter: generate JSON payload
 exporter->>V: rmdir timestamped dir
-exporter->>insights-operator: JSON HTTP response
+exporter->>http-client: JSON HTTP response
 deactivate exporter
 ```
--- insights-runtime-exporter/cmd/exporter/main.go
+++ insights-runtime-exporter/cmd/exporter/main.go
@@ -14,6 +14,10 @@ import (
 	"insights-runtime-exporter/pkg/utils"
 )
 
+const (
+	EXTRACTOR_ADDRESS string = "127.0.0.1:3000"
+)
+
 // gatherRuntimeInfo will trigger a new extraction of runtime info
 // and reply with a JSON payload
 func gatherRuntimeInfo(w http.ResponseWriter, r *http.Request) {
@@ -22,31 +26,25 @@ func gatherRuntimeInfo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// connect to the exporter server on the loopback address
-	conn, err := net.Dial("tcp", "127.0.0.1:3000")
+	dataPath, err := trigger_runtime_info_extraction()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer conn.Close()
-
-	// trigger a runtime extraction by opening a connection to the exporter server
-	fmt.Fprintf(conn, "")
-
-	dataPath, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
+	_, err = os.Stat(dataPath)
+	if dataPath == "" || os.IsNotExist(err) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	dataPath = strings.TrimSpace(dataPath)
+	defer os.RemoveAll(dataPath)
 	fmt.Println("Reading runtime info data from :", dataPath)
 
 	payload, err := collect_workload_payload(dataPath)
-	fmt.Println("Payload:", payload)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	fmt.Println("Payload:", payload)
 
 	response, err := json.Marshal(payload)
 	if err != nil {
@@ -55,91 +53,101 @@ func gatherRuntimeInfo(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(response)
+}
 
-	// remove the dataPath directory
+func trigger_runtime_info_extraction() (string, error) {
+	conn, err := net.Dial("tcp", EXTRACTOR_ADDRESS)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	// write to TCP connection to trigger a runtime extraction
+	fmt.Fprintf(conn, "")
+
+	dataPath, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(dataPath), nil
 }
 
 func collect_workload_payload(dataPath string) (types.NodeRuntimeInfo, error) {
 	// create a map with the key being "${pod-namespace}-${pod-name}-${container-id}"
 	payload := make(types.NodeRuntimeInfo)
 
-	fmt.Printf("Reading data from |%s|\n", dataPath)
 	// Read all directory entries (1 per running container)
 	entries, err := os.ReadDir(dataPath)
 	if err != nil {
 		return nil, err
 	}
 	for _, entry := range entries {
-		if entry.IsDir() {
-			containerDir := filepath.Join(dataPath, entry.Name())
+		if !entry.IsDir() {
+			continue
+		}
 
-			// read the file container-info.txt to get the pod-name, pod-namespace, container-id fields
-			info, exists := utils.ReadPropertiesFile(filepath.Join(containerDir, "container-info.txt"))
-			if !exists {
-				continue
-			}
-			namespace := info["pod-namespace"]
-			podName := info["pod-name"]
-			containerID := info["container-id"]
+		containerDir := filepath.Join(dataPath, entry.Name())
 
-			fmt.Println("Reading info for container:", namespace, podName, containerID)
+		// read the file container-info.txt to get the pod-name, pod-namespace, container-id fields
+		info, exists := utils.ReadPropertiesFile(filepath.Join(containerDir, "container-info.txt"))
+		if !exists {
+			continue
+		}
+		namespace := info["pod-namespace"]
+		podName := info["pod-name"]
+		containerID := info["container-id"]
 
-			runtimeInfo := types.ContainerRuntimeInfo{}
-			osFingerprintPath := filepath.Join(containerDir, "os.txt")
-			if info, exists := utils.ReadPropertiesFile(osFingerprintPath); exists {
-				runtimeInfo.OSReleaseID = info["os-release-id"]
-				runtimeInfo.OSReleaseVersionID = info["os-release-version-id"]
-			}
-			runtimeKindPath := filepath.Join(containerDir, "runtime-kind.txt")
-			if info, exists := utils.ReadPropertiesFile(runtimeKindPath); exists {
-				runtimeInfo.RuntimeKind = info["runtime-kind"]
-				runtimeInfo.RuntimeKindVersion = info["runtime-kind-version"]
-				runtimeInfo.RuntimeKindImplementer = info["runtime-kind-implementer"]
-			}
+		fmt.Println("Reading info for container:", namespace, podName, containerID)
 
-			// Read the directory
-			entries, err := os.ReadDir(containerDir)
-			if err != nil {
-				continue
-			}
+		runtimeInfo := types.ContainerRuntimeInfo{}
+		osFingerprintPath := filepath.Join(containerDir, "os.txt")
+		if info, exists := utils.ReadPropertiesFile(osFingerprintPath); exists {
+			runtimeInfo.OSReleaseID = info["os-release-id"]
+			runtimeInfo.OSReleaseVersionID = info["os-release-version-id"]
+		}
+		runtimeKindPath := filepath.Join(containerDir, "runtime-kind.txt")
+		if info, exists := utils.ReadPropertiesFile(runtimeKindPath); exists {
+			runtimeInfo.RuntimeKind = info["runtime-kind"]
+			runtimeInfo.RuntimeKindVersion = info["runtime-kind-version"]
+			runtimeInfo.RuntimeKindImplementer = info["runtime-kind-implementer"]
+		}
 
-			var runtimes []types.RuntimeComponent
+		if _, exists := payload[namespace]; !exists {
+			payload[namespace] = make(types.NamespaceRuntimeInfo)
+		}
+		if _, exists := payload[namespace][podName]; !exists {
+			payload[namespace][podName] = make(types.PodRuntimeInfo)
+		}
+		payload[namespace][podName][containerID] = runtimeInfo
 
-			for _, file := range entries {
-				if !file.IsDir() && strings.HasSuffix(file.Name(), "-fingerprints.txt") {
-					if info, exists := utils.ReadPropertiesFile(filepath.Join(containerDir, file.Name())); exists {
-						for k, v := range info {
-							runtimes = append(runtimes, types.RuntimeComponent{
-								Name:    k,
-								Version: v,
-							})
-						}
+		// Read all other fingerprints files to fill the runtimes map
+		var runtimes []types.RuntimeComponent
+		entries, err := os.ReadDir(containerDir)
+		if err != nil {
+			continue
+		}
+		for _, file := range entries {
+			if !file.IsDir() && strings.HasSuffix(file.Name(), "-fingerprints.txt") {
+				if info, exists := utils.ReadPropertiesFile(filepath.Join(containerDir, file.Name())); exists {
+					for k, v := range info {
+						runtimes = append(runtimes, types.RuntimeComponent{
+							Name:    k,
+							Version: v,
+						})
 					}
 				}
 			}
-			runtimeInfo.Runtimes = runtimes
-
-			if _, exists := payload[namespace]; !exists {
-				fmt.Println("create entry for namespace", namespace)
-				payload[namespace] = make(types.NamespaceRuntimeInfo)
-			}
-			if _, exists := payload[namespace][podName]; !exists {
-				fmt.Println("create entry for namespace pod", namespace, podName)
-				payload[namespace][podName] = make(types.PodRuntimeInfo)
-			}
-			payload[namespace][podName][containerID] = runtimeInfo
 		}
+		runtimeInfo.Runtimes = runtimes
 	}
 
 	return payload, nil
 }
 
 func main() {
-	// Set up HTTP handlers for specific paths.
 	http.HandleFunc("/gather-runtime-info", gatherRuntimeInfo)
 
-	// Start the HTTP server on port 3000.
-	fmt.Println("Starting server at port 8000")
+	fmt.Println("Starting exporter HTTP server at port 8000")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		fmt.Println(err)
 	}
